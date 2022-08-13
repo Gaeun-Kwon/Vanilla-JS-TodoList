754ac5413fe898b50150e95b26593c7ec88c6a5f
권가은 <aileen1007@ewhain.net>
2022-08-13 12:00:30 +0900

    ~ #7.5

---
+++ js/todo.js
@@ -0,0 +1,45 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.querySelector("#todo-form input");
+const todoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let todos = [];
+
+function saveTodos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+function paintTodo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+} 
+
+function handleTodoSubmit(event){
+    event.preventDefault();    
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    todos.push(newTodo);
+    paintTodo(newTodo);
+    saveTodos();
+}
+todoForm.addEventListener("submit", handleTodoSubmit);
+
+
+const savedTodos = localStorage.getItem(TODOS_KEY); 
+
+if (savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos); 
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
