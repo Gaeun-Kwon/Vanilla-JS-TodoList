b345b7fd2de700b38f7ecbb0704f5b0e556de3f9
권가은 <aileen1007@ewhain.net>
2022-08-14 16:48:36 +0900

    ~ #8.

--- js/todo.js
+++ js/todo.js
@@ -12,11 +12,14 @@ function saveTodos(){
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter(todo => todo.id !== parseInt(li.id)); 
+    saveTodos();
 }
 function paintTodo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id; 
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text; 
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
@@ -29,8 +32,12 @@ function handleTodoSubmit(event){
     event.preventDefault();    
     const newTodo = todoInput.value;
     todoInput.value = "";
-    todos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = { 
+        text: newTodo,
+        id: Date.now() 
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj); 
     saveTodos();
 }
 todoForm.addEventListener("submit", handleTodoSubmit);
@@ -43,3 +50,4 @@ if (savedTodos !== null){
     todos = parsedTodos;
     parsedTodos.forEach(paintTodo);
 }
+
