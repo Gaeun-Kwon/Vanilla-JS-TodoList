a9faa957e53838b0243590aacfc40a07de219309
권가은 <aileen1007@ewhain.net>
2022-09-17 19:51:46 +0900

    backgroud img 시간별 적용

--- js/background.js
+++ js/background.js
@@ -1,8 +1,24 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.png", "1.png", "2.png", "3.png", "4.png"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
+//const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
+
+const date = new Date();
+const hours = date.getHours();
+
+let chosenImage = 0;
+
+if (hours >= 0 && hours <5) {
+  chosenImage = images[0];
+} else if (hours >= 5 && hours < 9){
+  chosenImage = images[1];
+} else if (hours >= 9 && hours < 17){
+  chosenImage = images[2];
+} else if (hours >= 17 && hours < 21){
+  chosenImage = images[3];
+} else {
+  chosenImage = images[4];
+}
 
 bgImage.id = "bgImg";
 bgImage.src = `img/${chosenImage}`; 
