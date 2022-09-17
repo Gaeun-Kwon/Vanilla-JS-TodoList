0e4b6eae371e629f802ce0549722859f5232d3d5
권가은 <aileen1007@ewhain.net>
2022-09-17 19:17:11 +0900

    weather widget

--- js/weather.js
+++ js/weather.js
@@ -3,14 +3,22 @@ const API_KEY = "75d545aa2684c63d1fb8a256d9e07959"
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
+    //console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; 
     fetch(url)
         .then(reponse => reponse.json())
         .then(data => {
-            const weather = document.querySelector("#weather span:first-child")
-            const city = document.querySelector("#weather span:last-child")
+            const weatherImg = document.querySelector("#weather img")
+            const city = document.querySelector("#weather span:first-of-type")
+            const tempCurr = document.querySelector("#weather span:nth-of-type(2)")
+            const weather = document.querySelector("#weather span:nth-of-type(3)")
+            const tempMaxMin = document.querySelector("#weather span:last-of-type")
+
+            weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
             city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            tempCurr.innerText = `${data.main.temp}°`;
+            weather.innerText = data.weather[0].main;
+            tempMaxMin.innerText = `최고:${data.main.temp_max}° 최저:${data.main.temp_min}°`;
         }); 
 }
 function onGeoError(){
