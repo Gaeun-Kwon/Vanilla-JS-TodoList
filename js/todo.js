f869c0d0bd080c4e58e3cfc46b2ccf3c92b684e0
권가은 <aileen1007@ewhain.net>
2022-09-18 23:42:17 +0900

    완료시 체크박스 숨김

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,18 @@ const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
+const DONE_TODOS_KEY = "doneTodos";
 
 let todos = [];
+let doneArr = [];
 
 function saveTodos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
+function saveDoneTodos(){
+    localStorage.setItem(DONE_TODOS_KEY, JSON.stringify(doneArr));
+}
+
 function deleteTodo(event){
     //클릭된 버튼의 list 화면에서 제거 후, 다시 추가해 화면에서 밑으로 내려간 것처럼 표현
     const li = event.target.parentElement;
@@ -18,26 +24,40 @@ function deleteTodo(event){
         id: li.id
     };
     li.remove();
-    paintTodo(doneTodoObj);
+    paintDoneTodo(doneTodoObj);
 
     //filter 이용해서 클릭된 버튼의 list 포함x인 array, 포함o인 array 만든 후
     //concat 함수로 합쳐 todos 에 저장
     const temp = todos.filter(todo => todo.id !== parseInt(li.id));
-    const doneArr = todos.filter(todo => todo.id == parseInt(li.id));
+    doneArr = todos.filter(todo => todo.id == parseInt(li.id));
     todos = temp.concat(doneArr);
     saveTodos();
+    saveDoneTodos();
 }
 function paintTodo(newTodo){
     const li = document.createElement("li");
     li.id = newTodo.id; 
     const span = document.createElement("span");
     span.innerText = newTodo.text; 
-    const blank = document.createElement("span");
-    blank.innerText = " ";
     const button = document.createElement("button");
     button.innerText = "완료";
     button.addEventListener("click", deleteTodo);
     li.appendChild(button);
+    li.appendChild(span);
+    todoList.appendChild(li);
+} 
+
+function paintDoneTodo(newTodo){
+    const li = document.createElement("li");
+    li.id = newTodo.id; 
+    const span = document.createElement("span");
+    span.innerText = newTodo.text; 
+    /*
+    const button = document.createElement("button");
+    button.innerText = "완료";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(button);
+    */
     li.appendChild(span);
     todoList.appendChild(li);
 } 
