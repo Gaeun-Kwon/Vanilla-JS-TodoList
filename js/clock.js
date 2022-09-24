99887ae721d5a1509b73c3fd3d23e89253c283c6
권가은 <aileen1007@ewhain.net>
2022-09-25 08:22:44 +0900

    요일 오류 수정

--- js/clock.js
+++ js/clock.js
@@ -2,13 +2,13 @@ const clock = document.querySelector("#clock");
 const today = document.querySelector("#today");
 
 function getClock(){
-    const week = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"];
+    const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = `${hours}:${minutes}:${seconds}`;
-    today.innerText = `${date.getMonth() + 1}월 ${date.getDate()}일 ${week[date.getDay() - 1]}`;
+    today.innerText = `${date.getMonth() + 1}월 ${date.getDate()}일 ${week[date.getDay()]}`;
 }
 
 getClock();
